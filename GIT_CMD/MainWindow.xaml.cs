27b85212f731986a88fbb5b2a3382243b2602ab9
nmd974 <jha.payet@gmail.com>
2021-10-22 08:46:47 +0400

    Ajout de la selection du path

--- GIT_CMD/MainWindow.xaml.cs
+++ GIT_CMD/MainWindow.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System.Windows;
 using System.Diagnostics;
-using System.Windows.Forms;
+using Microsoft.WindowsAPICodePack.Dialogs;
 
 namespace GIT_CMD
 {
@@ -14,53 +14,63 @@ namespace GIT_CMD
             InitializeComponent();
         }
 
-        private void Select_Direction(object sender, RoutedEventArgs e)
-        {
-            using (var fbd = new FolderBrowserDialog())
-            {
-                DialogResult result = fbd.ShowDialog();
 
-                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
-                {
-                    string[] files = Directory.GetFiles(fbd.SelectedPath);
-
-                    System.Windows.Forms.MessageBox.Show("Files found: " + files.Length.ToString(), "Message");
-                }
-            }
-        }
         private void Cloner(object sender, RoutedEventArgs e)
         {
-            string link_folder = "cd " + folder_link.Text;
-            string command = "git clone " +  link_repo.Text;
-            
-            var startInfo = new System.Diagnostics.ProcessStartInfo
+            if(folder_link.Text == "")
             {
-               // WorkingDirectory = @"J:\Projets\C#\APPS",
-                WindowStyle = System.Diagnostics.ProcessWindowStyle.Normal,
-                FileName = "cmd.exe",
-                RedirectStandardInput = true,
-                UseShellExecute = false,
-            };
-
-            try
-            {
-                Process cmd = Process.Start(startInfo);
-                cmd.StandardInput.WriteLine(link_folder);
-                cmd.StandardInput.WriteLine(command);
-                cmd.StandardInput.Close();
-                cmd.WaitForExit();
-                MessageBox.Show("Le repo a bien été cloné !");
-                folder_link.Clear();
-                link_repo.Clear();
-                System.Environment.Exit(0);
+                MessageBox.Show("Veuillez renseigner le dossier où cloner ce repo !");
             }
-            catch (System.ComponentModel.Win32Exception win32Exception)
+            else if(link_repo.Text == "")
             {
-                //The system cannot find the file specified...
-                Debug.WriteLine(win32Exception.Message);
-                MessageBox.Show(win32Exception.Message);
+                MessageBox.Show("Veuillez renseigner le repo à cloner !");
+            }
+            else
+            {
+                string link_folder = "cd " + folder_link.Text;
+                string command = "git clone " + link_repo.Text;
+
+                var startInfo = new System.Diagnostics.ProcessStartInfo
+                {
+                    // WorkingDirectory = @"J:\Projets\C#\APPS",
+                    WindowStyle = System.Diagnostics.ProcessWindowStyle.Normal,
+                    FileName = "cmd.exe",
+                    RedirectStandardInput = true,
+                    UseShellExecute = false,
+                };
+
+                try
+                {
+                    Process cmd = Process.Start(startInfo);
+                    cmd.StandardInput.WriteLine(link_folder);
+                    cmd.StandardInput.WriteLine(command);
+                    cmd.StandardInput.Close();
+                    cmd.WaitForExit();
+                    MessageBox.Show("Le repo a bien été cloné !");
+                    folder_link.Clear();
+                    link_repo.Clear();
+                    System.Environment.Exit(0);
+                }
+                catch (System.ComponentModel.Win32Exception win32Exception)
+                {
+                    //The system cannot find the file specified...
+                    Debug.WriteLine(win32Exception.Message);
+                    MessageBox.Show(win32Exception.Message);
+                }
+            }
+            
+        }
+        private void SelectDirection(object sender, RoutedEventArgs e)
+        {
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.InitialDirectory = "C:\\Users";
+            dialog.IsFolderPicker = true;
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                MessageBox.Show("You selected: " + dialog.FileName);
             }
         }
+
 
     }
 }
